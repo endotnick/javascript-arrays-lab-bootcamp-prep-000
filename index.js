41e1a73e50e793f0aaac0bbad2574ad9016baf18
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-12 20:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten() {
-  
+
 }
 
 function destructivelyPrependKitten() {
-  
+
 }
 
 function destructivelyRemoveFirstKitten() {
-  
+
 }
 
 function destructivelyRemoveLastKitten() {
-  
+
 }
 
 function appendKitten() {
-  
+
 }
 
 function prependKitten() {
-  
+
 }
 
 function removeFirstKitten() {
-  
+
 }
 
 function removeLastKitten() {
-  
+
 }
